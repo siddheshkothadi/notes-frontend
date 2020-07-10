139f05b2b2baad10fea4a9ff1a9a7335f56ac12b
siddheshkothadi <realsiddheshko@gmail.com>
2020-07-10 13:52:18 +0530

    added delete button to delete note

--- src/components/Note.js
+++ src/components/Note.js
@@ -3,9 +3,31 @@ import './Note.css'
 
 export default function Note(props){
     return(
-        <div className='Note' onClick={()=>{console.log(props._id)}}>
+        <div className='Note'>
             <div className='Note-Title'>{props.title}</div>
             <div className='Note-Description'>{props.description}</div>
+            <div className='Note-DeleteSection'>
+                <img src='delete.png' alt='delete' onClick={()=>deleteNote(props._id)}/>
+            </div>
         </div>
     )
+}
+
+function deleteNote(_id){
+    fetch(`http://localhost:5000/notes/${_id}`,
+        {
+            method: "DELETE",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin":true
+            },
+            mode: "cors"
+        })
+        .then(()=>{
+            window.location.reload()
+        })
+        .catch(err => {
+            throw(err)
+        });
 }
