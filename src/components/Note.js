a9f81dc7c4effbcaa1a4f4ce3cd603a7066679f4
siddheshkothadi <realsiddheshko@gmail.com>
2020-07-10 20:48:33 +0530

    added put function

--- src/components/Note.js
+++ src/components/Note.js
@@ -3,13 +3,17 @@ import './Note.css'
 
 export default function Note(props){
     return(
-        <div className='Note'>
-            <div className='Note-Title'>{props.title}</div>
-            <div className='Note-Description'>{props.description}</div>
-            <div className='Note-DeleteSection'>
-                <img src='delete.png' alt='delete' onClick={()=>deleteNote(props._id)}/>
+        <div className='Note-Modal'>
+            <div className='Note' > 
+                <div className='Note-Title'>{props.title}</div>
+                <div className='Note-Description'>{props.description}</div>
+                <div className='Note-DeleteSection'>
+                    <img src='delete.png' alt='delete' onClick={()=>deleteNote(props._id)}/>
+                </div>
             </div>
         </div>
+            
+        
     )
 }
 
@@ -23,6 +27,29 @@ function deleteNote(_id){
                 "Access-Control-Allow-Origin":true
             },
             mode: "cors"
+        })
+        .then(()=>{
+            window.location.reload()
+        })
+        .catch(err => {
+            throw(err)
+        });
+}
+
+function updateNote(_id,title,description) {
+    fetch(`http://localhost:5000/notes/update/${_id}`,
+        {
+            method: "PUT",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin":true
+            },
+            mode: "cors",
+            body: JSON.stringify({
+                title: title,
+                description: description
+            })
         })
         .then(()=>{
             window.location.reload()
