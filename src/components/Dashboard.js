c3321d7291a9b136113b4523811ae278cabbc2d3
siddheshkothadi <realsiddheshko@gmail.com>
2020-07-15 21:26:29 +0530

    Added animations

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -11,10 +11,14 @@ export default function Dashboard(props){
     )
     else if (props.isSignedIn===false) return (
         <div className='NotSignedInDashboard'>
-            <img className='Notes-Logo' src={process.env.PUBLIC_URL + '/favicon.ico'} alt='Icon'/>
+            <div className='Notes-Head'>
+                <img className='Notes-Logo' src={process.env.PUBLIC_URL + '/favicon.ico'} alt='Icon'/>
+                <div className='Notes-Head-Text'>NOTES</div>
+            </div>
+            
             <div className='Typewriter'>
                 <div>The simplest way</div>
-                <div>to keep notes.....</div>
+                <div>to keep notes ...</div>
             </div>
             <div className="Google-Btn" onClick={()=>window.location.href='http://localhost:5000/auth/google'}>
                 <img className="Google-Icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt='google'/>
