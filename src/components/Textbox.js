0c2192a4fc775bc1a0aca75fa70d569513249323
siddheshkothadi <realsiddheshko@gmail.com>
2020-07-19 14:21:50 +0530

    added submitNote function

--- src/components/Textbox.js
+++ src/components/Textbox.js
@@ -5,22 +5,7 @@ export default function Textbox(props){
     const [title,setTitle] = useState('')
     const [description,setDescription] = useState('')
 
-    return(
-        <div className='Container'>
-            <input className='Title' type='text' spellCheck='false' placeholder='Title' onChange={e => setTitle(e.target.value)}/>
-            <textarea className='Description' type='text' spellCheck='false' placeholder='Take a note...' onChange={e => setDescription(e.target.value)}/>
-            {title || description ? (
-                <div className='Submit' onClick={()=>{
-                    submitNote(props.user.googleId,title,description)
-                }}>Add Note</div>
-            ) : (
-                <div className='Submit-Null'>Add Note</div>
-            )}
-        </div>
-    )
-}
-
-function submitNote(googleId,title,description){
+    function submitNote(googleId,title,description){
         fetch("http://localhost:5000/notes/add",
         {
             method: "POST",
@@ -42,4 +27,19 @@ function submitNote(googleId,title,description){
         .catch(err => {
             throw(err)
         });
+    }
+
+    return(
+        <div className='Container'>
+            <input className='Title' type='text' spellCheck='false' placeholder='Title' onChange={e => setTitle(e.target.value)}/>
+            <textarea className='Description' type='text' spellCheck='false' placeholder='Take a note...' onChange={e => setDescription(e.target.value)}/>
+            {title || description ? (
+                <div className='Submit' onClick={()=>{
+                    submitNote(props.user.googleId,title,description)
+                }}>Add Note</div>
+            ) : (
+                <div className='Submit-Null'>Add Note</div>
+            )}
+        </div>
+    )
 }
